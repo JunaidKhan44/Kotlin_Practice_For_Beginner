35dcfd4bed3baab7957a2a97604d2cfa955db675
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-02-04 15:32:01 +0500

    Source Code.
    Source Code.

---
+++ src/main/kotlin/Visiblity_Modifier.kt
@@ -0,0 +1,30 @@
+fun main(){
+    //visibility modifier
+}
+
+//by default nothing written it is public
+//public
+//class AA
+//var b=20
+//fun gn(){}
+
+//internal is same as privous but only differece is accessable with in same module
+//internal class AA
+//var b=20
+//fun gn(){}
+
+
+//in case of private not accessable outside the same class
+//private class AA
+//var b=20
+//fun gn(){}
+
+//propected not use in top level declaration
+//protected class AA    //show error
+//var b=20
+//protected fun gn(){}
+
+
+
+
+
